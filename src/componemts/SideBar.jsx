ceea36bf5364a415a5635b8e5a4017288624f43a
parasrajain <parasrajain7@gmail.com>
2025-03-14 13:34:37 +0530

    final update

--- src/componemts/SideBar.jsx
+++ src/componemts/SideBar.jsx
@@ -13,22 +13,22 @@ const SideBar = () => {
         aToken && <ul className='text-[#515151] mt-5'>
           <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]':''}`} to={'/admin-dashboard'}> 
             <img src={assets.home_icon} alt="" />
-            <p>Dashboard</p>
+            <p className='hidden md:block'>Dashboard</p>
           </NavLink>
 
           <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]':''}`} to={'/appointments'}>
             <img src={assets.appointment_icon} alt="" />
-            <p>Appointments</p>
+            <p className='hidden md:block'>Appointments</p>
           </NavLink>
 
           <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]':''}`} to={'/add-doctor'}>
             <img src={assets.add_icon} alt="" />
-            <p>Add Doctor</p>
+            <p className='hidden md:block'>Add Doctor</p>
           </NavLink>
 
           <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4 border-[#5f6FFF]':''}`} to={'/doctors-list'}>
             <img src={assets.people_icon} alt="" />
-            <p>Doctors List</p>
+            <p className='hidden md:block'>Doctors List</p>
           </NavLink>
         </ul>
       }
